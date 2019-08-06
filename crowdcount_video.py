e2bf86699ba6c8e3aecfae023e6200cfe41d7763
dinohub <dstadh17@gmail.com>
2019-08-06 11:00:04 +0800

    more configs

--- crowdcount_video.py
+++ crowdcount_video.py
@@ -10,7 +10,7 @@ from drawer import draw_count
 
 parser = argparse.ArgumentParser()
 parser.add_argument('video', help='video to process')
-parser.add_argument('--cfg', help='Config file to specify no count/deadzones')
+parser.add_argument('--cfg', help='Config file to specify no count/deadzones', default='ndp.cfg')
 args = parser.parse_args()
 
 assert os.path.exists(args.video),'Video does not exist!'
@@ -24,14 +24,11 @@ cap = cv2.VideoCapture(args.video)
 frame_w = cap.get(3)
 frame_h = cap.get(4)
 vid_fps = cap.get(5)
-# fourcc = cv2.VideoWriter_fourcc('H','2','6','4')
-fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
-# out_vid = cv2.VideoWriter(basename+'_cc.avi',fourcc, vid_fps, (int(frame_w), int(frame_h)))
 
 assert os.path.exists(args.cfg),'Config file given does not exist!'
 config = configparser.ConfigParser()
 config.read(args.cfg)
-# print(config.sections())
+
 skyline = int(config['SKYLINE']['Y'])
 skybuffer = 10
 skyline_polygon = [(0-skybuffer,0-skybuffer), (0-skybuffer,skyline), (frame_w-1+skybuffer, skyline), (frame_w-1+skybuffer, 0-skybuffer)]
@@ -48,9 +45,29 @@ omit_scales =[int(x) for x in config['VIDEO']['OmitScales'].split(',')]
 sample_rate_sec = float(config['VIDEO']['SampleRate'])
 sample_rate_frame = int(vid_fps * sample_rate_sec)
 
-out_csv = open(basename+'_output.csv','w')
-out_csv.write('TimeStamp(sec), FrameCount, PeopleCount\n')
+display = config['VIDEO'].getboolean('Display')
+outputVideo = config['VIDEO'].getboolean('OutputVideo')
+outputCSV = config['VIDEO'].getboolean('OutputCSV')
+if display:
+    print('Displaying outputs')
+    cv2.namedWindow('LSC-CNN', cv2.WINDOW_NORMAL)
+else: 
+    print('Hiding outputs')
 
+if outputVideo:
+    print('Outputing to video file')
+    # fourcc = cv2.VideoWriter_fourcc('H','2','6','4')
+    fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+    out_vid = cv2.VideoWriter(basename+'_cc.avi',fourcc, vid_fps, (int(frame_w), int(frame_h)))
+else:
+    print('Not outputing to video file')
+
+if outputCSV:
+    print('Outputing to CSV')
+    out_csv = open(basename+'_output.csv','w')
+    out_csv.write('TimeStamp(sec), FrameCount, PeopleCount\n')
+else:
+    print('Not outputing to CSV.')
 cc = CrowdCounter(frame_w,  frame_h, compress_ratio=compress_ratio, omit_scales=omit_scales, ignore_polys=dead_polygons)
 resized_dead_polygons = cc.ignore_polys_raw 
 
@@ -58,35 +75,43 @@ total_dur = 0
 frame_count = 0
 total_dur_count = 0
 # frame_skip = 1
-cv2.namedWindow('LSC-CNN', cv2.WINDOW_NORMAL)
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
-    frame_count += 1
-    # if frame_count%frame_skip != 0:
-    #     continue
-    if (frame_count-1)%sample_rate_frame != 0:
-        continue
-    tic = time.time()
-    show_img, count = cc.visualise_count(frame,)
-    print('Current crowd count: {}'.format(count))
-    draw_count(show_img, count, ignore_polys=resized_dead_polygons)
-    toc = time.time()
-    total_dur += (toc - tic)
-    total_dur_count += 1
-    # cv2.imwrite(os.path.join(out_dir,'{}.png'.format(frame_count)),frame)
-    out_vid.write(show_img)
-    out_text = '{},{},{}\n'.format(round(frame_count/vid_fps), frame_count, count)
-    # print(out_text)
-    out_csv.write(out_text)
-    cv2.imshow('LSC-CNN', show_img)
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        break
-    cv2.waitKey(5)
+try:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame_count += 1
+        # if frame_count%frame_skip != 0:
+        #     continue
+        if (frame_count-1)%sample_rate_frame != 0:
+            continue
+        tic = time.time()
+        show_img, count = cc.visualise_count(frame,)
+        print('Current crowd count @ {}s: {}'.format(round(frame_count/vid_fps), count))
+        if display:
+            draw_count(show_img, count, ignore_polys=resized_dead_polygons)
+        toc = time.time()
+        total_dur += (toc - tic)
+        total_dur_count += 1
+        # cv2.imwrite(os.path.join(out_dir,'{}.png'.format(frame_count)),frame)
+        if outputVideo:
+            out_vid.write(show_img)
+        if outputCSV:
+            out_text = '{},{},{}\n'.format(round(frame_count/vid_fps), frame_count, count)
+            # print(out_text)
+            out_csv.write(out_text)
+        if display:
+            cv2.imshow('LSC-CNN', show_img)
+            if cv2.waitKey(1) & 0xff == ord('q'):
+                break
+            cv2.waitKey(5)
+except Exception as e:
+    pass
+finally:
+    cap.release()
+    if outputVideo:
+        out_vid.release()
+    if outputCSV:
+        out_csv.close()
 
-cap.release()
-out_vid.release()
-out_csv.close()
-
-print('Avrg inference time:{}'.format(total_dur/total_dur_count))
+    print('Avrg inference time:{}'.format(total_dur/total_dur_count))
